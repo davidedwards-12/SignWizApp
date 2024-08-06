3c78a3ec17403a61f535afe410d1598424710c7b
David Edwards <dmedwards2002@gmail.com>
2024-08-05 20:48:50 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LearnSignLanguageApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
