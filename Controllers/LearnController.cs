466f37312696d4f8ae08ccccaa599d703e311e7d
David Edwards <dmedwards2002@gmail.com>
2024-08-07 12:24:20 -0500

    Started on the Learning page. Got Module 1 "finished"

---
+++ Controllers/LearnController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnSignLanguageApp.Controllers
+{
+    public class LearnController : Controller
+    {
+        public IActionResult Module1()
+        {
+            return View();
+        }
+    }
+}
