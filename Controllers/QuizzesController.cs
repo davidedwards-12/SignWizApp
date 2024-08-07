e6ef1fd7349e85548bb81cd29d063e8080e0728e
David Edwards <dmedwards2002@gmail.com>
2024-08-07 15:31:15 -0500

    Setting up how quizzes will look and work

---
+++ Controllers/QuizzesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearnSignLanguageApp.Controllers
+{
+    public class QuizzesController : Controller
+    {
+        public IActionResult QuizModule1()
+        {
+            return View();
+        }
+
+        public IActionResult QuizModule1Results()
+        {
+            return View();
+        }
+
+        public IActionResult QuizModule2()
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/QuizModule1ViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LearnSignLanguageApp.Models
+{
+    public class QuizModule1ViewModel
+    {
+        [Required]
+        public string Question1 { get; set; }
+
+        [Required]
+        public string Question2 { get; set; }
+
+        // Add more questions as needed
+    }
+}
