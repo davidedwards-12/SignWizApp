4ada50da4eacb814ee0516345ba82c793b2bdbdd
David Edwards <dmedwards2002@gmail.com>
2024-08-08 21:38:45 -0500

    Updated comments

--- Models/ApplicationUser.cs
+++ Models/ApplicationUser.cs
@@ -4,6 +4,10 @@ namespace LearnSignLanguageApp.Models
 {
     public class ApplicationUser : IdentityUser
     {
+        // The website does not get First or Last name from users.
+        // This was a test to see what and what does not work.
+        // Can maybe use the ProfilePictureUrl in the future.
+        
         //public string FirstName { get; set; }
         //public string LastName { get; set; }
         //public string ProfilePictureUrl { get; set; }
